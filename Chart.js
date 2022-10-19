fd30e94c773991a57d2da7053d8b3a52461a1bcb
BARUCH <B60617@gmail.com>
2022-10-20 01:26:36 +0300

    100%

--- Chart.js
+++ Chart.js
@@ -1,24 +1,26 @@
+function chart_activation(get, _nams) {
  
+  $('#chartContainer').hide();
+  const nams = _nams.map((n) => n.toUpperCase());
+  const arr_data = [];
+  
+  const dataPoints1 = [];
+  const dataPoints2 = [];
+  const dataPoints3 = [];
+  const dataPoints4 = [];
+  const dataPoints5 = [];
 
-function test(get) {
- 
+  const arr_dataPoints = [
+    dataPoints1,
+    dataPoints2,
+    dataPoints3,
+    dataPoints4,
+    dataPoints5
+  ];
 
-  test1(get);
-}
-
-function test1(get) {
-  let arr = [];
-
-
-  let dataPoints1 = [];
-  let dataPoints2 = [];
-  let dataPoints3 = [];
-  let dataPoints4 = [];
-  let dataPoints5 = [];
-
-  let options = {
+  const options = {
     title: {
-      text: "Electricity Generation in Turbine",
+      text: "Coins Live Report",
     },
     axisX: {
       title: "chart updates every 2 secs",
@@ -36,50 +38,21 @@ function test1(get) {
       fontColor: "dimGrey",
       itemclick: toggleDataSeries,
     },
-    data: [
-      {
-        type: "line",
-        xValueType: "dateTime",
-        yValueFormatString: "###.00Wh",
-        xValueFormatString: "hh:mm:ss TT",
-        showInLegend: true,
-        name: "",
-        dataPoints: dataPoints1,
-      },
-      {
-        type: "line",
-        xValueType: "dateTime",
-        yValueFormatString: "###.00Wh",
-        showInLegend: true,
-        name: "Turbine 2",
-        dataPoints: dataPoints2,
-      },
-      {
-        type: "line",
-        xValueType: "dateTime",
-        yValueFormatString: "###.00Wh",
-        showInLegend: true,
-        name: "Turbine 2",
-        dataPoints: dataPoints3,
-      },
-      {
-        type: "line",
-        xValueType: "dateTime",
-        yValueFormatString: "###.00Wh",
-        showInLegend: true,
-        name: "Turbine 2",
-        dataPoints: dataPoints4,
-      },
-      {
-        type: "line",
-        xValueType: "dateTime",
-        yValueFormatString: "###.00Wh",
-        showInLegend: true,
-        name: "Turbine 2",
-        dataPoints: dataPoints5,
-      },
-    ],
+    data: [],
   };
+
+  arr_dataPoints.forEach((e, i) => {
+  options.data.push({
+    type: "line",
+    xValueType: "dateTime",
+    yValueFormatString: "###.00$",
+    showInLegend: true,
+    name: nams[i],
+    dataPoints: e,
+  })
+});
+options.data[0].xValueFormatString  = "hh:mm:ss TT";
+  
 
   let chart = $("#chartContainer").CanvasJSChart(options);
 
@@ -93,14 +66,9 @@ function test1(get) {
   }
 
   let updateInterval = 2000;
-  // initial value
-
   let time = new Date();
-
-  // starting at 10.00 am
-
   let tHours = time.getHours();
-  let tMinutes = time.getMinutes() - 4;
+  let tMinutes = time.getMinutes() - 3;
   let tSeconds = time.getSeconds();
   let tMilliseconds = time.getMilliseconds();
 
@@ -109,91 +77,49 @@ function test1(get) {
   time.setSeconds(tSeconds);
   time.setMilliseconds(tMilliseconds);
 
-  let nams = [];
-
- 
-
   function updateChart(count) {
     $.ajax({
       url: `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${get}&tsyms=USD`,
       success: function (e) {
-        // console.log(e);
-        arr.splice(0, arr.length);
-        nams.splice(0, nams.length);
+        arr_data.splice(0, arr_data.length);
         for (const obj in e) {
-          nams.push(obj);
-          arr.push(e[obj]);
+          arr_data.push(e[obj]);
         }
       },
-      erorr: function (erorr){
+      erorr: function (erorr) {
         console.log(erorr);
-      }
+      },
     });
-    // console.log(arr);
     count = count || 1;
+    
     for (let i = 0; i < count; i++) {
       time.setTime(time.getTime() + updateInterval);
-
-      // adding random value and rounding it to two digits.
-
-      // pushing the new values
-
-      if (arr[0]) {
-        let key1 = Object.keys(arr[0]);
-        // console.log(arr[0]);
-        dataPoints1.push({
-          x: time.getTime(),
-          y: arr[0][key1],
-        });
-      }
-
-      if (arr[1]) {
-        let key2 = Object.keys(arr[1]);
-        dataPoints2.push({
-          x: time.getTime(),
-          y: arr[1][key2],
-        });
-      }
-
-      if (arr[2]) {
-        let key3 = Object.keys(arr[2]);
-        dataPoints3.push({
-          x: time.getTime(),
-          y: arr[2][key3],
-        });
-      }
-
-      if (arr[3]) {
-        let key4 = Object.keys(arr[3]);
-        dataPoints4.push({
-          x: time.getTime(),
-          y: arr[3][key4],
-        });
-      }
-
-      if (arr[4]) {
-        let key5 = Object.keys(arr[4]);
-        dataPoints5.push({
-          x: time.getTime(),
-          y: arr[4][key5],
-        });
-      }
+      let key;
+      arr_dataPoints.forEach((e, i) => {
+        if (arr_data[i]) {
+          key = Object.keys(arr_data[i]);
+          e.push({
+            x: time.getTime(),
+            y: arr_data[i][key],
+          });
+    }
+    });
     }
 
-    for(let i = 0; i <= 4; i++){
-      if (arr[i]) {
-        let key = Object.keys(arr[i]);
-        options.data[i].legendText = `${nams[i]}: ${arr[i][key]}$`;
+    for (let i = 0; i <= 4; i++) {
+      if (arr_data[i]) {
+        let key = Object.keys(arr_data[i]);
+        options.data[i].legendText = `${nams[i]}: ${arr_data[i][key]}$`;
       }
     }
 
     $("#chartContainer").CanvasJSChart().render();
   }
-  
+
   updateChart(100);
   setInterval(function () {
     updateChart();
   }, updateInterval);
-}
-
-
+    $('#app').hide();
+    $('#chartContainer').show(4000);
+ };
--- index.js
+++ index.js
@@ -1,75 +1,73 @@
-let app = document.querySelector("#app");
-let arr_currency = [];
-let arr_names = [];
-(() => {
-  loader();
+const app = document.querySelector("#app");
+const my_chart = document.querySelector("#chartContainer");
 
-  function loader() {
-    app.innerHTML = `<span class="loader_home">Loading</span>`;
+const arr_coins = [];
+const coins_of_reports = [];
+// (() => {
+my_chart.style.display = "none";
+display_loader();
+
+function display_loader() {
+  app.innerHTML = `<div class="concentration"><span class="loader_home">Loading</span></div>`;
+};
+
+let currencies = [];
+
+get_data();
+
+async function get_data() {
+  try {
+    const response = await fetch("https://api.coingecko.com/api/v3/coins/");
+    currencies = await response.json();
+    obj_coins(currencies);
+  } catch (error) {
+    display_loader();
   }
+};
 
-  let currencies = [];
-
-  get_data();
-
-  async function get_data() {
-    try {
-      const response = await fetch("https://api.coingecko.com/api/v3/coins/");
-      currencies = await response.json();
-      obj_p(currencies);
-    } catch (error) {
-      loader();
-    }
+function obj_coins(currencies) {
+  for (let i = 0; i < 50; i++) {
+    let obj = {};
+    obj.id = currencies[i].id;
+    obj.name = currencies[i].name;
+    obj.symbol = currencies[i].symbol;
+    arr_coins.push(obj);
   }
+  print_currencies(arr_coins);
+};
 
-  function obj_p(currencies) {
-    for (let i = 0; i < 50; i++) {
-      let obj = {};
-      obj.id = currencies[i].id;
-      obj.name = currencies[i].name;
-      obj.symbol = currencies[i].symbol;
-      arr_currency.push(obj);
-    }
-    // console.log(arr_currency);
-    print_currencies(arr_currency);
-  }
-
-  function print_currencies(arr_currency) {
-    let html = "";
-    arr_currency.forEach((currency) => {
-      html += `
-  <div id="${currency.id}" class="card coins" style="width: 18rem;">
+function print_currencies(arr_coins) {
+  let html = '<section id="home_coins">';
+  arr_coins.forEach((coins) => {
+    html += `
+  <div id="${coins.id}" class="card coins" style="width: 18rem;">
   <div class="card-body">
-  
-    <h5 class="card-title">${currency.symbol}<label class="switch">
-    <input type="checkbox" id="check${currency.id}"  onclick="checkbox_add_list('${currency.id}')">
+    <h5 class="card-title">${coins.symbol.toUpperCase()}<label class="switch">
+    <input type="checkbox" id="check${coins.id}"  onclick="checkbox_add_list('${coins.id}')">
     <span class="slider round"></span>
     </label></h5>
-    
-    <p class="card-text">${currency.name}   </p>
-
-    <p>
-  <button onclick="get_info('${currency.id}')"    class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapse_${currency.id}"  id=btn${currency.name}> More Info</button>
-  </p>
-  <div class="collapse" id="collapse_${currency.id}">
-  <div class="card card-body concentration" id="info${currency.id}">
-  
-  </div>
-  </div>
-
-  </div>
-  </div>
+    <p class="card-text">${coins.name}   </p>
+    <p><button onclick="get_info('${coins.id}')"    
+  class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapse_${coins.id}" 
+   id=btn${coins.name}> More Info</button> </p>
+  <div class="collapse" id="collapse_${coins.id}">
+  <div class="card card-body concentration" id="info${coins.id}">
+  </div> </div> </div> </div>
   `;
-      arr_names.push(currency.name);
-    });
-
-    app.innerHTML = html;
+  });
+  html += '</section>'
+  app.innerHTML = html;
+  
+  for(let i = 0; i < coins_of_reports.length; i++){
+    arr_coins.forEach(coins => {
+      let checkbox = document.querySelector(`#check${coins.id}`);
+     coins.id === coins_of_reports[i].id && (checkbox.checked = true );
+  });
   }
-})();
+};
+// })();
 
 function get_info(id) {
-  // document.querySelectorAll('.collapse').hidden();
-  // document.querySelector(`#collapse_${id}`).show;
   if (sessionStorage.getItem(id) === null) {
     get_info_api(id);
   } else {
@@ -83,7 +81,7 @@ function get_info(id) {
       get_info_api(id);
     }
   }
-}
+};
 
 async function get_info_api(id) {
   let div_info = document.querySelector(`#info${id}`);
@@ -91,13 +89,12 @@ async function get_info_api(id) {
   try {
     const response = await fetch(`https://api.coingecko.com/api/v3/coins/${id}`);
     const info = await response.json();
-    // console.log(info);
     print_info(info, id);
     cache(id, info);
   } catch (error) {
-    console.log(error);
+    div_info.innerHTML = `<span class="loader"></span>`;
   }
-}
+};
 
 function print_info(info, id) {
   let div_info = document.querySelector(`#info${id}`);
@@ -108,98 +105,118 @@ function print_info(info, id) {
 <span> ILS: ${info.market_data.high_24h.ils}&#8362;</span>
 `;
   div_info.innerHTML = html;
-}
+};
 
 function cache(id, info) {
   info.time_is_up = new Date().getTime() + 120000;
   let info_json = JSON.stringify(info);
   sessionStorage.setItem(id, info_json);
-}
+};
 
-$(function () {
-  let availableTags = arr_names;
+
+
+function search() {
+  event.preventDefault();
+  display({ id:"btn_home" });
+  const name_coins = $("#tags");
+  const coins = $(".coins");
+  if (name_coins.val()) {
+    coins.hide();
+    coins_of_reports.forEach((coins) => coins.symbol === name_coins.val() && $(`#${coins.id}`).show());
+  }
+  name_coins.val("");
+};
+
+
+
+function checkbox_add_list(id) {
+  let checkbox = document.querySelector(`#check${id}`);
+  if (checkbox.checked) {
+    if (coins_of_reports.length === 5) {
+      let html = `
+      <section class ="concentration">
+      <div id="pop_up">
+        <p>Maximum To Choose: 5</p>
+        <ul id="ul_pop" class="list-group">`;
+      coins_of_reports.forEach((coins) => {
+        html += `<li class="list-group-item" >
+          <input class="form-check-input me-1" type="checkbox" checked value="" id="inp_pop${coins.id}" onchange="deleting_coins_report('${coins.id}', '${id}')">
+          <label class="form-check-label pointer" for="inp_pop${coins.id}">${coins.name}</label>
+          </li>`;
+      });
+      html += `</ul> <button class="btn btn-primary" onclick="close_popup()">Close</button></div> </section>`;
+      app.innerHTML += html;
+      coins_of_reports.forEach((coins) => (checkbox = document.querySelector(`#check${coins.id}`).checked = true));
+    } else {
+      arr_coins.forEach((coins) => coins.id === id && coins_of_reports.push(coins));
+    }
+  } else {
+    coins_of_reports.forEach((coins, i) => coins.id === id && coins_of_reports.splice(i, 1));
+    close_popup();
+  }
+};
+
+document.querySelector('#tags').addEventListener('focus', () => {
+  let arr = [];
+  coins_of_reports.forEach(e => {
+    arr.push(e.symbol);
+  });
+  let availableTags = arr;
   $("#tags").autocomplete({
     source: availableTags,
   });
 });
 
-function search(){
-  event.preventDefault();
-  let name = $("#tags").val();
-  if (name) {
-    $(".coins").hide();
-    arr_currency.forEach(function (currency) {
-      if (currency.name === name) {
-        $(`#${currency.id}`).show();
-      }
-    });
-  } else {
-    $(".coins").show();
-  }
-}
 
-let arr_currency_of_reports = [];
-
-function checkbox_add_list(id) {
-  let checkbox = document.querySelector(`#check${id}`);
-  if (checkbox.checked) {
-    if (arr_currency_of_reports.length === 5) {
-      let html = `
-      <div id="pop_up">
-        <p>Maximum to choose: 5</p>
-        <ul id="ul_pop" class="list-group">`;
-        arr_currency_of_reports.forEach(currency => {
-          html += `<li class="list-group-item" >
-          <input class="form-check-input me-1" type="checkbox" checked value="" id="inp_pop${currency.id}" onchange="deleting_currency_report('${currency.id}')">
-          <label class="form-check-label x" for="inp_pop${currency.id}">${currency.name}</label>
-          </li>`
-          // document.querySelector(`#inp${currency.id}`).checked = true;
-        });
-            html += `</ul> <button class="btn btn-primary" onclick="close_popup()">Close</button></div>`;
-      app.innerHTML += html;
-      arr_currency_of_reports.forEach((currency) => (checkbox = document.querySelector(`#check${currency.id}`).checked = true));
-    } else {
-      arr_currency.forEach((currency) => {
-        if (currency.id === id) {
-          arr_currency_of_reports.push(currency);
-        }
-      });
-    }
-  } else {
-    for (let i = 0; i < arr_currency_of_reports.length; i++) {
-      if (arr_currency_of_reports[i].id === id) {
-        arr_currency_of_reports.splice(i, 1);
-      }
-    }
-    close_popup();
-  }
-}
-function close_popup(){
+function close_popup() {
   $("#pop_up").remove();
-}
-function deleting_currency_report(id) {
-  let checkbox = document.querySelector(`#check${id}`);
-  checkbox.checked = false;
+};
+
+function deleting_coins_report(id, elm) {
+  document.querySelector(`#check${id}`).checked = false;
+  document.querySelector(`#check${elm}`).checked = true;
   checkbox_add_list(id);
-}
+  checkbox_add_list(elm)
+};
 
-function activate_reports(){
-  let get = '';  
-  arr_currency_of_reports.forEach(currency => {
-    get += `${currency.symbol},`
+function activate_reports() {
+  const arr_names = [];
+  let get = "";
+  coins_of_reports.forEach((coins) => {
+    get += `${coins.symbol},`;
+    arr_names.push(coins.symbol);
   });
-  if(get){
-      get.slice(0, -1);
-  // get_reports(get);
-  $('#app').hide();
-  test(get)
+
+  if((!get)){
+    alert('add currencies to the report!');
+    return;
   }
+  get.slice(0, -1);
+  document.body.style.backgroundImage = "none";
+  chart_activation(get, arr_names);
+  console.log(arr_names);
+};
 
-}
+function display_about() {
+  return `
+  <section id="about" class="concentration">
+  <img src="./images/nft-_background.png" alt="logo" width="130">
+    <h2 > CRYPTONITE</h2>
+    <span class="separator"></span>
+    <p>In this application you will receive information about digital currencies, currency prices in dollars, euros and shekels.</p> <p> You can add coins to a data chart that displays live coin prices.</p>
+    <span class="separator"></span>
+    <p>My name is Baruch Kaminer and i am a front-end developer, an expert in building websites and applications and i have extensive experience and knowledge in innovative technologies and many programming languages. i developed this site using html, css, javascript and jQury.</p>
+    <span class="separator"></span>
+    <p id="contact">Contact me by email: <a href="mailto:b60617@gmail.com">B60617@gmail.com</a> </p>
+</section>`;
+};
 
-// async function get_reports(get){
-//   const response = await fetch(`https://min-api.cryptocompare.com/data/pricemulti?fsyms=${get}&tsyms=USD`);
-//   const data = await response.json();
-//   console.log(data);
-// }
+function display(e) {
+  e.id === "btn_home" ? print_currencies(arr_coins) : (app.innerHTML = display_about());
+  my_chart.style.display = "none";
+  app.style.display = "flex";
+};
 
+document.querySelector('h1').addEventListener('click', () => location.reload() );
+document.querySelector('#btn_search').addEventListener('click', search);
+document.querySelector('#btn_reports').addEventListener('click', activate_reports);
